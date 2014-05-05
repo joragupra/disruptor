8364aae52eabc2199c90544bfeac2015e801b1f7
Jorge Agudo Praena <joragupra@gmail.com>
2014-05-05 16:02:06 +0200

    Anonymous lambdas substituted by method references

--- src/main/java/com/joragupra/disruptorSample/LongEventMain.java
+++ src/main/java/com/joragupra/disruptorSample/LongEventMain.java
@@ -8,17 +8,29 @@ import java.util.concurrent.Executors;
 
 public class LongEventMain  {
 
+    public static LongEvent createLongEvent() {
+        return new LongEvent();
+    }
+
+    public static void handleEvent(LongEvent event, long sequence, boolean endOfBatch) {
+        System.out.println(event);
+    }
+
+    public static void translate(LongEvent event, long sequence, ByteBuffer buffer) {
+        event.set(buffer.getLong(0));
+    }
+
     public static void main( String[] args ) throws Exception {
         Executor executor = Executors.newCachedThreadPool();
         int bufferSize = 1024;
-        Disruptor<LongEvent> disruptor = new Disruptor<>(() -> new LongEvent(), bufferSize, executor);
-        disruptor.handleEventsWith((event, sequence, endOfBatch) -> System.out.println("Event: " + event));
+        Disruptor<LongEvent> disruptor = new Disruptor<>(LongEventMain::createLongEvent, bufferSize, executor);
+        disruptor.handleEventsWith(LongEventMain::handleEvent);
         disruptor.start();
         RingBuffer<LongEvent> ringBuffer = disruptor.getRingBuffer();
         ByteBuffer bb = ByteBuffer.allocate(8);
         for (long l = 0; true; l++) {
         	bb.putLong(0, l);
-        	ringBuffer.publishEvent((event, sequence, buffer) -> event.set(buffer.getLong(0)), bb);
+        	ringBuffer.publishEvent(LongEventMain::translate, bb);
         	Thread.sleep(1000);
         }
     }
